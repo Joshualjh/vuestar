7665603b7e6b777ab1b5e00651cbf508f53f93be
Joshualjh <aak1009@naver.com>
2022-05-10 13:56:53 +0900

    image change

---
+++ src/components/ContainerItems.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <PostItem :dataItems="dataItems[i]" v-for="(a, i) in dataItems" :key="i" />
+    <!-- <PostItem :dataItems="dataItems[1]" />
+    <PostItem :dataItems="dataItems[2]" /> -->
+  </div>
+</template>
+
+<script>
+import PostItem from "./PostItems.vue";
+export default {
+  name: "ContainerItems",
+  components: {
+    PostItem,
+  },
+  props: {
+    dataItems: Array,
+  },
+};
+</script>
+
+<style>
+</style>
